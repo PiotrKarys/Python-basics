a447ee8b7dbb8935a063bd0898268e771a55de41
Piotr Karyś <138370309+PiotrKarys@users.noreply.github.com>
2024-10-04 11:40:18 +0200

    Create first.py

---
+++ first.py
@@ -0,0 +1,50 @@
+# Zadanie 1: Wprowadzenie do zmiennych i wyświetlania tekstu
+
+# 1. Poproś użytkownika o podanie swojego imienia
+
+# 2. Poproś użytkownika o podanie swojego wieku
+
+# 3. Wyświetl powitanie używając podanego imienia
+
+# 4. Wyświetl informację o wieku
+
+# 5. Stwórz zmienną z twoim ulubionym kolorem - będzie ona od razu przypisana
+your_favourite_color = "tu wpisz swój ulubiony kolor"
+
+# 6. Wyświetl informację o ulubionym kolorze
+
+
+# Bonus: Użyj f-string do wyświetlenia wszystkich informacji w jednym zdaniu
+
+# Rozwiązanie zadania znajduje się poniżej. Spróbuj najpierw samodzielnie, zanim spojrzysz na kod!
+input("Press Enter to continue...")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+"""
+imie = input("Podaj swoje imię: ")
+wiek = input("Podaj swoj wiek: ")
+print("Cześć, " + imie + "!")
+print("Masz " + wiek + " lat.")
+ulubiony_kolor  = "niebieski"
+print("Mój ulubiony kolor to " + ulubiony_kolor + ".")
+print(f"Cześć {imie}! Masz {wiek} lat, a mój ulubiony kolor to {ulubiony_kolor}.")
+"""
